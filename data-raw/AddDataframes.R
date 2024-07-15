549e40e2aea6d1368d11c113294cbd1a01173325
Ashlynn Ivy Wimer <ashewimer@gmail.com>
2024-07-15 13:19:34 -0500

    #2 but with marginally better compression

--- data-raw/AddDataframes.R
+++ data-raw/AddDataframes.R
@@ -49,11 +49,11 @@ C_2000   <- counties[3]
 C_1990   <- counties[4]
 C_1980   <- counties[5]
 
-use_data(C_Latest) 
-use_data(C_2010)   
-use_data(C_2000)   
-use_data(C_1990)   
-use_data(C_1980)   
+use_data(C_Latest, compress='xz') 
+use_data(C_2010,   compress='xz')   
+use_data(C_2000,   compress='xz')   
+use_data(C_1990,   compress='xz')   
+use_data(C_1980,   compress='xz')   
 
 states <- state |> 
   map(grabDataFromGithub, base_url=BASE_URL) |>
@@ -65,11 +65,11 @@ S_2000   <- states[3]
 S_1990   <- states[4]
 S_1980   <- states[5]
 
-use_data(S_Latest) 
-use_data(S_2010)   
-use_data(S_2000)   
-use_data(S_1990)   
-use_data(S_1980)   
+use_data(S_Latest, compress='xz') 
+use_data(S_2010,   compress='xz')   
+use_data(S_2000,   compress='xz')   
+use_data(S_1990,   compress='xz')   
+use_data(S_1980,   compress='xz')   
 
 tracts <- tract |> 
   map(grabDataFromGithub, base_url=BASE_URL) |>
@@ -81,15 +81,15 @@ T_2000   <- tracts[3]
 T_1990   <- tracts[4]
 T_1980   <- tracts[5]
 
-use_data(T_Latest) 
-use_data(T_2010)   
-use_data(T_2000)   
-use_data(T_1990)   
-use_data(T_1980)   
+use_data(T_Latest, compress='xz') 
+use_data(T_2010,   compress='xz')   
+use_data(T_2000,   compress='xz')   
+use_data(T_1990,   compress='xz')   
+use_data(T_1980,   compress='xz')   
 
 zctas <- zcta |> 
   map(grabDataFromGithub, base_url=BASE_URL) |>
   map(padGeoids, size=5)
 
 Z_Latest <- zctas[1]
-use_data(Z_Latest)
+use_data(Z_Latest, compress='xz')
