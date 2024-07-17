0e6ed2ec0bbf0f4f91c80962f65ac6c1bbfce4b1
Ashlynn Ivy Wimer <ashewimer@gmail.com>
2024-07-17 10:40:40 -0500

    initial function, added geography support #4

--- R/load_oeps.R
+++ R/load_oeps.R
@@ -1,23 +1,48 @@
 #' @export 
-load_oeps <- function(year, scale, geography=FALSE) {
+load_oeps <- function(scale, year, geometry=FALSE) {
 
-  validate_request(year, scale)
-  
-  return(eval(parse(text=make_file_name(year, scale))))
-}
-
-make_file_name <- function(year, scale) {
-  geog_letter <- toupper(substr(scale, 1, 1))
-  year_tag <- paste0(toupper(substr(year, 1, 1)), substr(year, 2, 7))
-  return(paste(geog_letter, year_tag, sep="_"))
-}
-
-validate_request <- function(year, scale) {
-  valid_years = c("1980", "1990", "2000", "2010", "latest")
-  valid_scales = c("county", "state", "zcta'", "tract")
-  
   stopifnot(
-    tolower(as.character(year)) %in% valid_years,
-    tolower(scale) %in% valid_scales
+    grepl('state|tract|county|zcta|counties', scale, ignore.case=T),
+    grepl('1980|1990|2000|2010|latest', year, ignore.case=T)
   )
+  
+  attribute_data <- eval(parse(text=make_file_name(scale, year)))
+  
+  if (!geometry) {
+    return(attribute_data)
+  }
+  
+  geometry <- retrieve_geometry(scale, quiet=TRUE)
+  
+  data <- merge(attribute_data, geometry, on='HEROP_ID')
+  
+  return(data)
+}
+
+make_file_name <- function(scale, year) {
+  scale <- toupper(substr(scale, 1, 1))
+  year <- paste0(toupper(substr(year, 1, 1)), substr(year, 2, 7))
+  return(paste(scale, year, sep="_"))
+}
+
+retrieve_geometry <- function(scale, quiet=FALSE) {
+  shape_url <- '/vsicurl/https://raw.githubusercontent.com/GeoDaCenter/opioid-policy-scan/main/data_final/geometryFiles/'
+
+  if (grepl('state', scale)) {
+    shape_url <- paste0(shape_url, 'state/states2010.shp')
+  }
+  
+  if (grepl('tract', scale)) {
+    shape_url <- paste0(shape_url, 'tract/tracts2010.shp')
+  }
+  
+  if (grepl('county|counties', scale)) {
+    shape_url <- paste0(shape_url, 'county/counties2010.shp')
+  }
+  
+  if (grepl('zcta', scale)) {
+    shape_url <- paste0(shape_url, 'zcta/zctas2010.shp')
+  }
+  
+  return(st_read(shape_url, quiet=quiet))
 }
