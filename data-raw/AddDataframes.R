8ad3811b8b48e127b765eae79478a23d74c8072b
Ashlynn Ivy Wimer <ashewimer@gmail.com>
2024-07-17 10:38:56 -0500

    fixed save format

--- data-raw/AddDataframes.R
+++ data-raw/AddDataframes.R
@@ -43,11 +43,11 @@ counties <- county |>
   map(padGeoids, size=5)
 
 # `use_data` only accepts named objects
-C_Latest <- counties[1]
-C_2010   <- counties[2]
-C_2000   <- counties[3]
-C_1990   <- counties[4]
-C_1980   <- counties[5]
+C_Latest <- counties[[1]]
+C_2010   <- counties[[2]]
+C_2000   <- counties[[3]]
+C_1990   <- counties[[4]]
+C_1980   <- counties[[5]]
 
 use_data(C_Latest, compress='xz') 
 use_data(C_2010,   compress='xz')   
@@ -59,11 +59,11 @@ states <- state |>
   map(grabDataFromGithub, base_url=BASE_URL) |>
   map(padGeoids, size=2) 
 
-S_Latest <- states[1]
-S_2010   <- states[2]
-S_2000   <- states[3]
-S_1990   <- states[4]
-S_1980   <- states[5]
+S_Latest <- states[[1]]
+S_2010   <- states[[2]]
+S_2000   <- states[[3]]
+S_1990   <- states[[4]]
+S_1980   <- states[[5]]
 
 use_data(S_Latest, compress='xz') 
 use_data(S_2010,   compress='xz')   
@@ -75,11 +75,11 @@ tracts <- tract |>
   map(grabDataFromGithub, base_url=BASE_URL) |>
   map(padGeoids, size=11)
 
-T_Latest <- tracts[1]
-T_2010   <- tracts[2]
-T_2000   <- tracts[3]
-T_1990   <- tracts[4]
-T_1980   <- tracts[5]
+T_Latest <- tracts[[1]]
+T_2010   <- tracts[[2]]
+T_2000   <- tracts[[3]]
+T_1990   <- tracts[[4]]
+T_1980   <- tracts[[5]]
 
 use_data(T_Latest, compress='xz') 
 use_data(T_2010,   compress='xz')   
@@ -91,5 +91,5 @@ zctas <- zcta |>
   map(grabDataFromGithub, base_url=BASE_URL) |>
   map(padGeoids, size=5)
 
-Z_Latest <- zctas[1]
+Z_Latest <- zctas[[1]]
 use_data(Z_Latest, compress='xz')
