c19676b99ac81c8e055c698921989ae75bd3229e
Ashlynn Ivy Wimer <ashewimer@gmail.com>
2024-07-24 13:22:14 -0500

    restructured helper functions

--- R/attribute_formatting.R
+++ R/attribute_formatting.R
@@ -1,59 +1,12 @@
-#' Standardize scale
+#' Merge keys
 #'
-#' Take user defined scale and try to map it to one of a selection of possible 
-#' inputs.
-#'
-#' @param scale String containing messy scale whose identity needs guessed.
-standardize_scale <- function(scale) {
-  if (grepl("state", scale, ignore.case = TRUE)) {
-    return("state")
-  }
-  
-  if (grepl("tract", scale, ignore.case = TRUE)) {
-    return("tract")
-  }
-  
-  if (grepl("county|counties", scale, ignore.case = TRUE)) {
-    return("county")
-  }
-  
-  if (grepl("zcta", scale, ignore.case = TRUE)) {
-    return("zcta")
-  }
+#' Returns the vector of merge keys used by all data within this package.
+#' 
+#' @returns A vector of strings (the names of the merge key columns).
+merge_keys <- function() { 
+  merge_keys <- c("HEROP_ID", "GEOID", "TRACTCE", "STATEFP", "COUNTYFP") 
 }
 
-#' Retrieve OEPS geometries
-#' 
-#' Download OEPS geometries of a specified scale.
-#' 
-#' @param scale String specifying geographic scale to pull from.
-#' One of 'state', 'tract', 'county', or 'zcta'
-#' @param quiet Boolean specifying whether to output st_read console output.
-#' Defaults to FALSE.
-#' 
-#' @import sf
-#' @returns Simple feature collection containing the specified geometries.
-retrieve_geometry <- function(scale, quiet = FALSE) {
-  shape_url <- "/vsicurl/https://raw.githubusercontent.com/GeoDaCenter/opioid-policy-scan/main/data_final/geometryFiles/"
-  
-  if (scale == "state") {
-    shape_url <- paste0(shape_url, "state/states2010.shp")
-  }
-  
-  if (scale == "tract") {
-    shape_url <- paste0(shape_url, "tract/tracts2010.shp")
-  }
-  
-  if (scale == "county") {
-    shape_url <- paste0(shape_url, "county/counties2010.shp")
-  }
-  
-  if (scale == "zcta") {
-    shape_url <- paste0(shape_url, "zcta/zctas2010.shp")
-  }
-  
-  return(sf::st_read(shape_url, quiet = quiet))
-}
 
 #' Tidify non-merge key variables
 #'
@@ -76,14 +29,6 @@ tidify_data <- function(df) {
   return(tidy_df)
 }
 
-#' Merge keys
-#'
-#' Returns the vector of merge keys used by all data within this package.
-#' 
-#' @returns A vector of strings (the names of the merge key columns).
-merge_keys <- function() { 
-  merge_keys <- c("HEROP_ID", "GEOID", "TRACTCE", "STATEFP", "COUNTYFP") 
-}
 
 #' Filter by state
 #'
@@ -99,7 +44,7 @@ filter_by_state <- function(df, states) {
   # TODO: add a reference to a look-up table to parse abbreviations, state names
   
   stopifnot("HEROP_ID" %in% names(df))
-
+  
   if (length(states) > 1) {
     return(Reduce(rbind, lapply(states, filter_by_state, df=df)))
   }
@@ -107,6 +52,7 @@ filter_by_state <- function(df, states) {
   state_fp <- gsub(" ", "0", sprintf("%2s", states))
   return(df[substr(df$HEROP_ID, 6, 7) == state_fp,])
 }
+
 
 #' Filter by county
 #'
